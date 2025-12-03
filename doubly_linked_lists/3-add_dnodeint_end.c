fee1ecf4960c4c0f6788c19f8c3826e90e9f6ebe
JAuxance <auxance.jourdan@proton.me>
2025-12-03 16:17:02 +0100

    fix: improve comments and formatting in add_dnodeint_end function

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,15 +1,17 @@
 #include "lists.h"
 /**
- *
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list
+ * @head: pointer to the head of the list
+ * @n: integer to be added to the new node
+ * Return: address of the new element, or NULL if it failed
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *node = malloc(sizeof(dlistint_t));
 	dlistint_t *temp;
+
 	if (node == NULL)
-	{
 		return (NULL);
-	}
 
 	node->n = n;
 	node->prev = NULL;
@@ -20,12 +22,14 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		*head = node;
 		return (node);
 	}
+
 	temp = *head;
+
 	while (temp->next != NULL)
-	{
 		temp = temp->next;
-	}
+
 	temp->next = node;
 	node->prev = temp;
 	node->next = NULL;
+	return (node);
 }
