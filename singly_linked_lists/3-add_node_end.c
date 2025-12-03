cb6fc5802709754fa7a6e1d3c390e4a7ddf3cb29
JAuxance <auxance.jourdan@proton.me>
2025-12-03 13:19:25 +0100

    refactor: streamline node creation and initialization in add_node_end function

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -9,11 +9,11 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	unsigned int len = 0;
 	list_t *node = malloc(sizeof(list_t));
+	list_t *temp;
 
 	if (node == NULL)
-	{
 		return (NULL);
-	}
+
 	node->str = strdup(str);
 	if (node->str == NULL)
 	{
@@ -21,23 +21,22 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 	while (str[len] != '\0')
-	{
 		len++;
-		node->len = len;
-	}
+
+	node->len = len;
+	node->next = NULL;
+
 	if (*head == NULL)
 	{
-		node->next;
 		*head = node;
 		return (node);
 	}
-	list_t *temp = *head;
-
+	temp = *head;
 	while (temp->next != NULL)
 	{
 		temp = temp->next;
 	}
 	temp->next = node;
-	node->next = NULL;
+
 	return (node);
 }
