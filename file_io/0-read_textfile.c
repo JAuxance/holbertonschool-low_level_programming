17ab893c3dc79431ebb8a64a1fb97b3e091d204f
JAuxance <auxance.jourdan@proton.me>
2025-12-05 12:33:40 +0100

    fix: refactor read_textfile function to use low-level I/O and improve error handling

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,34 +1,52 @@
 #include "main.h"
+
 /**
- * read_textfile - Reads a text file and prints
- * @filename: The name of the file to read.
- * @letters: The number of letters to read and print.
+ * read_textfile - Reads a text file and prints it to POSIX standard output
+ * @filename: The name of the file to read
+ * @letters: The number of letters it should read and print
  *
- * Return: The actual number of letters read and printed,
- *         or 0 if the file cannot be opened or read,
- *         or if filename is NULL.
+ * Return: The actual number of letters it could read and print,
+ *         0 if the file cannot be opened or read,
+ *         0 if filename is NULL,
+ *         0 if write fails or does not write the expected amount of bytes
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *file;
+	int fd;
 	char *buffer;
-	size_t bytesRead;
+	ssize_t bytes_read, bytes_written;
 
 	if (filename == NULL)
 		return (0);
-	file = fopen(filename, "r");
-	if (file == NULL)
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-	buffer = (char *)malloc(sizeof(char) * (letters + 1));
+
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
-		fclose(file);
+		close(fd);
 		return (0);
 	}
-	bytesRead = fread(buffer, sizeof(char), letters, file);
-	buffer[bytesRead] = '\0';
-	fwrite(buffer, sizeof(char), bytesRead, stdout);
+
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
 	free(buffer);
-	fclose(file);
-	return (bytesRead);
+	close(fd);
+	return (bytes_written);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -1,9 +1,12 @@
 #ifndef FILE_IO_MAIN_H
 #define FILE_IO_MAIN_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
 #include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif /* FILE_IO_MAIN_H */
