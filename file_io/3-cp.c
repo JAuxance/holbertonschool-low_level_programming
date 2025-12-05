829472869fa654f775a719a35ef64f7568ef5ffa
JAuxance <auxance.jourdan@proton.me>
2025-12-05 12:58:38 +0100

    fix: simplify error handling and improve code readability in main function

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -25,51 +25,28 @@ int main(int argc, char *argv[])
 	char buffer[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
-	{
-		close(fd_from);
-		error_exit(99, "Error: Can't write to %s\n", argv[2]);
-	}
-
+		close(fd_from), error_exit(99, "Error: Can't write to %s\n", argv[2]);
 	while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written == -1 || bytes_written != bytes_read)
 		{
-			close(fd_from);
-			close(fd_to);
+			close(fd_from), close(fd_to);
 			error_exit(99, "Error: Can't write to %s\n", argv[2]);
 		}
 	}
-
 	if (bytes_read == -1)
-	{
-		close(fd_from);
-		close(fd_to);
-		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-	}
-
+		close(fd_from), close(fd_to), error_exit(98,
+			"Error: Can't read from file %s\n", argv[1]);
 	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
 	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
-
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
 	return (0);
 }
